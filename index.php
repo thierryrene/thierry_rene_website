ff5861d2c7c2dfdac9be1e9ed5b85677b4378248
thierryrenematos <td_matos@msn.com>
2017-09-02 21:57:17 -0300

    update

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
+
+require 'php/testando_composer/vendor/autoload.php';
+		
+	// if (!file_exists('cache' . $_SERVER['SCRIPT_NAME'])) {
+	// 	$cachefile = 'cache' . $_SERVER['SCRIPT_NAME'];
+	// 	fopen($cachefile, 'w');
+	// }
+
+	$cachefile = 'cache' . $_SERVER['SCRIPT_NAME'];
+
+	$cachetime = 120 * 60;
 	
+	if (file_exists($cachefile) && (time() - $cachetime < filemtime($cachefile))) {
+		include_once($cachefile);
+		echo "<!-- Cached " . date('d-m-Y H:i:s', filemtime($cachefile)) . " -->";
+		exit;
+	}
+
+	ob_start(); 	
+		
 	include_once "php/c.php";
 
 	logAccess();
@@ -23,6 +42,18 @@
 <?php
 
 	include_once "php/template/contact.php";
- 	include_once "php/template/footer.php";
+	include_once "php/template/footer.php";
+
+	$fp = fopen($cachefile, 'w');
+
+	$content = ob_get_contents();
+	
+	r($content);
+		
+	fwrite($fp, $content); 
+	
+	fclose($fp);
+	 
+	ob_end_flush();
 
 ?>
--- test.php
+++ test.php
@@ -2,10 +2,22 @@
 
 require_once 'php/c.php';
 
-fileOpen('text.txt');
+$url = "https://requestb.in/1m2y9cl1";
+$data = ['name' => 'thierryrene', 'email' => 'thierryrene@stealhelook.com.br'];
 
-echo "<hr>";
+$curl = curl_init($url);
 
-returnContent('main_content');
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+// curl_setopt($curl, CURLOPT_HTTPHEADER,
+//     ['Content-type: application/json']
+// );
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($curl);
+
+var_dump($result);
+
+curl_close($curl);
 
 ?>
