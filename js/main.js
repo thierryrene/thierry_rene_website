9c1cd6eacfc4a0870dd80feb398fd2b3cd87de53
thierry rene matos <td_matos@terra.com.br>
2017-12-28 16:12:30 +0000

    fix icons hr bug in sass

--- js/main.js
+++ js/main.js
@@ -7,28 +7,6 @@
 
 $(document).ready(function() {
 	
-	$('#thierry-photo, #name, #prof, #star-hr')
-		.addClass('animated flipInX')
-		.css({
-			'animation-duration': '3s'
-		});
-
-	$('#skills, #custom-navbar')
-		.addClass('animated fadeInUp')
-		.css({
-			'animation-duration': '3s'
-		});
-		
-	$('.music-link').hover(
-		function() {
-			$(this).addClass('animated fadeInUp');
-			console.log("AAAAAAAAH");
-		},
-		function() {
-			$(this).removeClass('animated fadeInUp');
-		}
-	);
-
 	navigator.serviceWorker && navigator.serviceWorker.register('./js/sw.js').then(function(registration) {
 		console.log('Excellent, registered with scope: ', registration.scope);
 	});
