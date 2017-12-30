2869ccafa7d7eec363320b6340ebd96c9adf4c39
thierry rene matos <td_matos@terra.com.br>
2017-12-30 18:27:25 +0000

    Update - remove contatct section

--- php/template/contact.php
+++ php/template/contact.php
@@ -2,60 +2,64 @@
 
 	<div class="container">
 
-		<div class="row">
+<!--		<div class="row">-->
 
-			<div class="col-md-12 text-center">
+<!--			<div class="col-md-12 text-center">-->
 
-				<h2>Get in touch!</h2>
+<!--				<h2>Get in touch!</h2>-->
 
-				<hr class="phone-hr">
+<!--				<hr class="phone-hr">-->
 
-				<div class="row">
+<!--				<div class="row">-->
 
-					<div class="col-md-offset-2 col-md-8">
+<!--					<div class="col-md-offset-2 col-md-8">-->
+						
+<!--						<link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">-->
 
-						<p class="lead">Precisa de alguma <strong>solução com foco na Web</strong> ou auxílio para desenvolver alguma idéia <strong>baseada na internet</strong>?</p>
-						<p class="lead">Entre em contato.
-							<br><img draggable="false" class="emoji" alt="😀" src="//twemoji.maxcdn.com/72x72/1f600.png"></p>
+<!--<i class="em em-some-emoji"></i>-->
 
-						<form class="form-horizontal" action="php/email/send_email.php" method="post">
+<!--						<p class="lead">Precisa de alguma <strong>solução com foco na Web</strong> ou auxílio para desenvolver alguma idéia <strong>baseada na internet</strong>?</p>-->
+<!--						<p class="lead">Entre em contato.-->
+<!--							<br><img draggable="false" class="emoji" alt="😀" src="//twemoji.maxcdn.com/72x72/1f600.png"></p>-->
 
-							<fieldset>
+<!--						<form class="form-horizontal" action="php/email/send_email.php" method="post">-->
+
+<!--							<fieldset>-->
 
 								<!-- nome input -->
-								<div class="form-group form-test">
-									<label class="">Primeiro me diga seu nome...</label>
-									<input id="name" name="name" type="text" class="form-control" required>
-								</div>
+<!--								<div class="form-group form-test">-->
+<!--									<label class="">Primeiro me diga seu nome...</label>-->
+<!--									<input id="name" name="name" type="text" class="form-control" required>-->
+<!--								</div>-->
 
 								<!-- Email input-->
-								<div class="form-group form-test">
-									<label class="">...o seu e-mail...</label>
-									<input id="email" name="email" type="text" class="form-control" required>
-								</div>
+<!--								<div class="form-group form-test">-->
+<!--									<label class="">...o seu e-mail...</label>-->
+<!--									<input id="email" name="email" type="text" class="form-control" required>-->
+<!--								</div>-->
 
 								<!-- Message body -->
-								<div class="form-group form-test">
-									<label class="">...e a sua ideia!</label>
-									<textarea class="form-control" id="message" name="message" rows="5" required></textarea>
-								</div>
+<!--								<div class="form-group form-test">-->
+<!--									<label class="">...e a sua ideia!</label>-->
+<!--									<textarea class="form-control" id="message" name="message" rows="5" required></textarea>-->
+<!--								</div>-->
 
 								<!-- Form actions -->
-								<div class="form-group">
-									<input type="submit" name="submit" id="submit" value="Enviar" class="btn btn-default btn-lg" />
-								</div>
+<!--								<div class="form-group">-->
+<!--									<input type="submit" name="submit" id="submit" value="Enviar" class="btn btn-default btn-lg" />-->
+<!--								</div>-->
 
-							</fieldset>
+<!--							</fieldset>-->
 
-						</form>
+<!--						</form>-->
 
-					</div>
+<!--					</div>-->
 
-				</div>
+<!--				</div>-->
 
-			</div>
+<!--			</div>-->
 
-		</div>
+<!--		</div>-->
 
 	</div>
 
