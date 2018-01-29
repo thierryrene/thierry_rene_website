d2f36c6f3f2a3d5138c23fe7c0fa39a58e0ed5a5
thierry rene matos <td_matos@terra.com.br>
2018-01-29 22:34:14 +0000

    test fb pixel addtocart

--- php/template/skills.php
+++ php/template/skills.php
@@ -6,23 +6,23 @@
 
 			<center>
 
-				<div class="col-sm-4 col-md-4 col-lg-4">
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 					<span>
-		              <i class="fa fa-desktop fa-5x"></i>
+		              <i class="fa fa-desktop fa-4x"></i>
 		              <h3>Design</h3>
 		            </span>
 				</div>
 
-				<div class="col-sm-4 col-md-4 col-lg-4">
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 					<span>
-		              <i class="fa fa-thumbs-o-up fa-5x"></i>
+		              <i class="fa fa-thumbs-o-up fa-4x"></i>
 		              <h3>UI & UX</h3>
 		            </span>
 				</div>
 
-				<div class="col-sm-4 col-md-4 col-lg-4">
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 					<span>
-		              <i class="fa fa-code fa-5x"></i>
+		              <i class="fa fa-code fa-4x"></i>
 		              <h3>Code</h3>
 		            </span>
 				</div>
