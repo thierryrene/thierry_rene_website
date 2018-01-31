74f9c8597617e76bfc78f57c2693732870e49901
thierry rene matos <td_matos@terra.com.br>
2018-01-31 00:27:06 +0000

    add css animations to intro

--- js/main.js
+++ js/main.js
@@ -6,9 +6,14 @@
  */
 
 $(document).ready(function() {
-	
+
 	navigator.serviceWorker && navigator.serviceWorker.register('./js/sw.js').then(function(registration) {
 		console.log('Excellent, registered with scope: ', registration.scope);
 	});
 
+	$('.navbar').addClass('animated fadeInUp').css('animation-delay', '1s');
+
+	$('header').addClass('animated fadeInUp').css('animation-delay', '2s');
+
 });
+
