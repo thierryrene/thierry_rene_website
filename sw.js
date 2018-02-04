cdccc401e3a5c8ae6a9247e55f83e9a85c4d9f0e
thierry rene matos <td_matos@terra.com.br>
2018-02-04 20:27:15 +0000

    pwa working, add to home OK

--- sw.js
+++ sw.js
@@ -1,15 +1,6 @@
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
+var cacheName = '';
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('the-magic-cache').then(function(cache) {
-      return cache.addAll([
+var filesToCache = [
         '/',
         'cache/index.html',
         'js/main.js',
@@ -18,7 +9,54 @@ self.addEventListener('install', function(e) {
         'img/photo.webp',
         'favicons/favicons.ico',
         'css/app.css'
-      ]);
+];
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        console.log(key);
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
+
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function(response) {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
+// self.addEventListener('install', function(e) {
+//   e.waitUntil(
+//     caches.open('the-magic-cache').then(function(cache) {
+//       return cache.addAll([
+//         '/',
+//         'cache/index.html',
+//         'js/main.js',
+//         'js/plugins.js',
+//         '/manifest.json',
+//         'img/photo.webp',
+//         'favicons/favicons.ico',
+//         'css/app.css'
+//       ]);
+//     })
+//   );
+// });
