79680c571f0abf0a07e7bfbb6a9e236668444054
thierry rene matos <td_matos@terra.com.br>
2018-02-07 00:38:05 +0000

    update service worker

--- sw.js
+++ sw.js
@@ -13,17 +13,17 @@ var filesToCache = [
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        console.log(key);
-        // if (key !== cacheName) {
-        //   console.log('[ServiceWorker] Removing old cache', key);
-        //   return caches.delete(key);
-        // }
-      }));
-    })
-  );
+  // e.waitUntil(
+  //   caches.keys().then(function(keyList) {
+  //     return Promise.all(keyList.map(function(key) {
+  //       console.log(key);
+  //       // if (key !== cacheName) {
+  //       //   console.log('[ServiceWorker] Removing old cache', key);
+  //       //   return caches.delete(key);
+  //       // }
+  //     }));
+  //   })
+  // );
   return self.clients.claim();
 });
 
