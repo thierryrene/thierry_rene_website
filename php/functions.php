09e7c5aa8765ddd1d0a43584ccb9af1d7c9a768d
thierry rene matos <td_matos@terra.com.br>
2018-02-08 02:35:36 +0000

    add support to google analytics api

--- php/functions.php
+++ php/functions.php
@@ -237,93 +237,93 @@ function getInsta($limit) {
 					return $photos;
 				}
 
-$analytics = initializeAnalytics();
-$profile = getFirstProfileId($analytics);
-$results = getResults($analytics, $profile);
+// $analytics = initializeAnalytics();
+// $profile = getFirstProfileId($analytics);
+// $results = getResults($analytics, $profile);
 
-function initializeAnalytics()
-{
-  // Creates and returns the Analytics Reporting service object.
+// function initializeAnalytics()
+// {
+//   // Creates and returns the Analytics Reporting service object.
 
-  // Use the developers console and download your service account
-  // credentials in JSON format. Place them in this directory or
-  // change the key file location if necessary.
-  $KEY_FILE_LOCATION = 'thierryrenewebsite-13b3166359af.json';
+//   // Use the developers console and download your service account
+//   // credentials in JSON format. Place them in this directory or
+//   // change the key file location if necessary.
+//   $KEY_FILE_LOCATION = 'thierryrenewebsite-13b3166359af.json';
 
-  // Create and configure a new client object.
-  $client = new Google_Client();
-  $client->setApplicationName("thierryrenewebsite_testes");
-  $client->setAuthConfig($KEY_FILE_LOCATION);
-  $client->setScopes(['https://www.googleapis.com/auth/analytics.readonly']);
-  $analytics = new Google_Service_Analytics($client);
+//   // Create and configure a new client object.
+//   $client = new Google_Client();
+//   $client->setApplicationName("thierryrenewebsite_testes");
+//   $client->setAuthConfig($KEY_FILE_LOCATION);
+//   $client->setScopes(['https://www.googleapis.com/auth/analytics.readonly']);
+//   $analytics = new Google_Service_Analytics($client);
 
-  return $analytics;
-}
+//   return $analytics;
+// }
 
-function getFirstProfileId($analytics) {
-  // Get the user's first view (profile) ID.
+// function getFirstProfileId($analytics) {
+//   // Get the user's first view (profile) ID.
 
-  // Get the list of accounts for the authorized user.
-  $accounts = $analytics->management_accounts->listManagementAccounts();
+//   // Get the list of accounts for the authorized user.
+//   $accounts = $analytics->management_accounts->listManagementAccounts();
 
-  if (count($accounts->getItems()) > 0) {
-    $items = $accounts->getItems();
-    $firstAccountId = $items[0]->getId();
+//   if (count($accounts->getItems()) > 0) {
+//     $items = $accounts->getItems();
+//     $firstAccountId = $items[0]->getId();
 
-    // Get the list of properties for the authorized user.
-    $properties = $analytics->management_webproperties
-        ->listManagementWebproperties($firstAccountId);
+//     // Get the list of properties for the authorized user.
+//     $properties = $analytics->management_webproperties
+//         ->listManagementWebproperties($firstAccountId);
 
-    if (count($properties->getItems()) > 0) {
-      $items = $properties->getItems();
-      $firstPropertyId = $items[0]->getId();
+//     if (count($properties->getItems()) > 0) {
+//       $items = $properties->getItems();
+//       $firstPropertyId = $items[0]->getId();
 
-      // Get the list of views (profiles) for the authorized user.
-      $profiles = $analytics->management_profiles
-          ->listManagementProfiles($firstAccountId, $firstPropertyId);
+//       // Get the list of views (profiles) for the authorized user.
+//       $profiles = $analytics->management_profiles
+//           ->listManagementProfiles($firstAccountId, $firstPropertyId);
 
-      if (count($profiles->getItems()) > 0) {
-        $items = $profiles->getItems();
+//       if (count($profiles->getItems()) > 0) {
+//         $items = $profiles->getItems();
 
-        // Return the first view (profile) ID.
-        return $items[0]->getId();
+//         // Return the first view (profile) ID.
+//         return $items[0]->getId();
 
-      } else {
-        throw new Exception('No views (profiles) found for this user.');
-      }
-    } else {
-      throw new Exception('No properties found for this user.');
-    }
-  } else {
-    throw new Exception('No accounts found for this user.');
-  }
-}
+//       } else {
+//         throw new Exception('No views (profiles) found for this user.');
+//       }
+//     } else {
+//       throw new Exception('No properties found for this user.');
+//     }
+//   } else {
+//     throw new Exception('No accounts found for this user.');
+//   }
+// }
 
 
 
-function getResults($analytics, $profileId) {
+// function getResults($analytics, $profileId) {
 
-  $t = date('Y-m-d', strtotime('-3000 days'));
-  $t2 = date('Y-m-d');
+//   $t = date('Y-m-d', strtotime('-3000 days'));
+//   $t2 = date('Y-m-d');
 
-  $params = array(
-              'max-results' => 10,
-              'dimensions' => 'ga:pagePath,ga:pageTitle',
-              'sort' => '-ga:pageviews',
-          );
+//   $params = array(
+//               'max-results' => 10,
+//               'dimensions' => 'ga:pagePath,ga:pageTitle',
+//               'sort' => '-ga:pageviews',
+//           );
 
-  $a = $analytics->data_ga->get(
-       'ga:' . $profileId,
-       $t,
-       $t2,
-       'ga:pageviews',
-       $params);
-r($a['rows']);
+//   $a = $analytics->data_ga->get(
+//       'ga:' . $profileId,
+//       $t,
+//       $t2,
+//       'ga:pageviews',
+//       $params);
+// r($a['rows']);
 
-   return $a['rows'];
-}
+//   return $a['rows'];
+// }
 
-$teste = getResults($analytics, $profile);
+// $teste = getResults($analytics, $profile);
 
 // function printResults($results) {
 //   // Parses the response from the Core Reporting API and prints
