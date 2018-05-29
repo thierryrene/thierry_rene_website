35515e89c2975b54b223869985dfa7731a0b64e7
thierryrenematos <td_matos@msn.com>
2018-05-29 01:16:12 -0300

    update

--- php/template/about.php
+++ php/template/about.php
@@ -71,7 +71,7 @@
 				<style type="text/css">
 				  	.swiper-container {
 				      width: 100%;
-				      height: auto;
+				      height: 200px;
 				    }
 				    .swiper-wrapper{
 				    	-webkit-transition-timing-function:linear!important;
