17151773d3bcc564fe5bd79ad3caa9287183cf9a
thierry rene matos <td_matos@terra.com.br>
2018-07-20 15:04:09 +0000

    update

--- testes/consulado/php/c.php
+++ testes/consulado/php/c.php
@@ -7,11 +7,24 @@ date_default_timezone_set('America/Sao_Paulo');
 // Using Medoo namespace
 use Medoo\Medoo;
 
-// Initialize
-$database = new Medoo([
-    'database_type' => 'mysql',
-    'database_name' => 'consulado_db',
-    'server' => '127.0.0.1',
-    'username' => 'root',
-    'password' => ''
-]);
+if ($_SERVER['HTTP_HOST'] != 'thierryrenematoswebdev.me') {
+    $database = new Medoo([
+        'database_type' => 'mysql',
+        'database_name' => 'consulado_db',
+        'server'        => '127.0.0.1',
+        'username'      => 'root',
+        'password'      => ''
+    ]);    
+} else {
+    $database = new Medoo([
+        'database_type' => 'mysql',
+        'database_name' => 'consulado_db',
+        'server'        => '127.0.0.1',
+        'username'      => 'root',
+        'password'      => '*casa123'
+    ]);
+}
+
+// echo "<pre>";
+// var_dump($_SERVER);
+// echo "</pre>";
